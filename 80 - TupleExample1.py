0ed20a9da8dbd5f8636102dec7120d7cb9b6fe2a
Dipaditya Das <DipadityaDas@gmail.com>
2020-02-17 22:47:03 +0530

    Modification of tuple file

--- 80 - TupleExample1.py
+++ 80 - TupleExample1.py
@@ -30,7 +30,7 @@ my_tuple = ("hello")
 print(type(my_tuple))  # <class 'str'>
 
 # Creating a tuple having one element
-my_tuple = ("hello")
+my_tuple = ("hello",)
 print(type(my_tuple))  # <class 'tuple'> 
 
 # Parentheses is optional
